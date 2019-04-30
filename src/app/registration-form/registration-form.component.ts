ee4758e728a29065d7711b62aee921d626a79845
Adam Jambor <ajjambor912@gmail.com>
2019-04-30 14:39:18 +0200

    refactor

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,32 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {API_CONFIG} from '../config/config.module';
-import {UserService} from "../services/user.service";
 
+import {UserParams, UserService} from "../services/user.service";
 
+import { DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent {
+
+export class RegistrationFormComponent{
 
 
+  user:UserParams = new UserParams();
   constructor(private http: HttpClient, private api: UserService) {
   }
 
-  user:Object ={
-    nick: null,
-    email: null,
-    plainPassword: null,
-    repeatPassword:null,
-    name: null,
-    age: null,
-    city: null,
-    sex: null,
-    image: null
-  }
+  errors : {email:'adres juz istnieje'};
+  imagePath:String = null;
 
   ngOnInit() {
     window.addEventListener('dragover', (e) => {
@@ -37,29 +30,40 @@ export class RegistrationFormComponent {
     }, false);
   }
   registerUser(){
-    this.api.registerUser(this.user);
+    console.log(this.user);
+    this.api.registerUser(this.user).subscribe(reg => {
+      if(!reg.status) this.errors = reg.error;
+    });
+  }
+  checkEmail(){
+    this.api.checkEmail(this.user.email);
   }
   checkNick(){
     this.api.checkNick(this.user.nick);
   }
+  checkPassword(){
+    this.api.checkPassword(this.user.plainPassword);
+  }
   handleDropImage(event) {
+
     event.preventDefault();
     let file = event.dataTransfer.files[0];
-    console.log(file);
-    let avatar = document.getElementById('avatar');
-    avatar.src = URL.createObjectURL(file);
-    this.image = file;
+    this.showImage(file);
+
   }
 
   getInputFile(event) {
-    let file = event.target.files;
-    let fReader = new FileReader();
-    fReader.onload = () => {
-      let avatar = document.getElementById('avatar');
-      avatar.src = fReader.result;
-    };
-    fReader.readAsDataURL(file[0]);
-    this.image = file;
+    let file = event.target.files[0];
+    this.showImage(file);
+
+
+  }
+  showImage(file){
+     let fReader = new FileReader();
+     fReader.onload = () =>{
+       this.imagePath = fReader.result.toString();
+     }
+    fReader.readAsDataURL(file);
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,37 +3,39 @@ import { HttpClient } from '@angular/common/http';
 
 import { User} from '../model/user/user.model';
 import {API_CONFIG} from '../config/config.module';
-interface checkEmailResponse {
+import {Observable} from "rxjs";
+class checkEmailResponse {
   status: boolean;
 }
-interface checkNickResponse{
+class checkNickResponse{
   status: boolean;
 }
-interface checkPasswordResponse{
+class checkPasswordResponse{
   status: boolean;
 }
-interface validation {
-  email: boolean,
-  nick: boolean,
-  password: boolean,
-  repeatPassword: boolean
+class registerUserResponse {
+  status:boolean;
+  error: [];
+}
+export class UserParams{
+
+  nick: String;
+  email: String;
+  plainPassword: String;
+  repeatPassword:String;
+  name: String;
+  age: String;
+  city: String;
+  sex: String;
+  image: String;
 }
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
   constructor(private http: HttpClient) { }
-  checkEmail(){
+  checkEmail(email){
     this.http.post<checkEmailResponse>(`${API_CONFIG.api}/security/check-email`, {
-      'email':this.email
-    }).subscribe( resp =>{
-      if(resp.status != true){
-        this.validation.email= false;
-
-      }else{
-        this.validation.email= true;
-
-      }
-      this.checkFields();
+      'email':email
     });
   }
   checkNick(nick){
@@ -41,41 +43,32 @@ export class UserService {
       'nick':nick
     }).subscribe( resp =>{
       if(resp.status != true){
-        this.validation.nick = false;
+
 
       }else{
-        this.validation.nick = true;
+
 
       }
-      this.checkFields();
+
     });
   }
-  checkPassword(){
+  checkPassword(plainPassword){
     this.http.post<checkPasswordResponse>(`${API_CONFIG.api}/security/check-password`, {
-      'nick':this.nick
+      'password':plainPassword
     }).subscribe( resp =>{
       if(resp.status != true){
-        this.validation.nick = false;
+
 
       }else{
-        this.validation.nick = true;
 
       }
-      this.checkFields();
-    });
-  }
-  checkRepeatPassword(){
-    if(this.plainPassword === this.repeatPassword){
-      this.validation.repeatPassword = true;
-    }else{
-      this.validation.repeatPassword = false;
-    }
-    this.checkFields();
-  }
-  registerUser(params) {
 
-    this.http.post(`${API_CONFIG.api}/security/register`, params).subscribe(resp => {
     });
+  }
+
+  registerUser(params) : Observable<registerUserResponse> {
+    
+    return this.http.post<registerUserResponse>(`${API_CONFIG.api}/security/register`, params);
   }
 
 }
